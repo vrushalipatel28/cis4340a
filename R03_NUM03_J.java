46c714be8a22afa4d9abaecc6d2210b3e9443abc
vrushalipatel28 <vrushalipatel2004@gmail.com>
2025-03-02 22:12:05 -0500

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,5 +1,5 @@
-public static int getInteger(DataInputStream is) throws IOException {
-  return is.readInt(); 
+public static long getInteger(DataInputStream is) throws IOException {
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
 
 public static long getInteger(DataInputStream is) throws IOException {

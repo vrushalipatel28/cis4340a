1f914b2852e08a9386520f437aaf540f4b3503be
vrushalipatel28 <vrushalipatel2004@gmail.com>
2025-03-02 21:56:47 -0500

    Update R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,8 +1,8 @@
 BigInteger x = new BigInteger("530500452766");
-byte[] byteArray = x.toByteArray();
-String s = new String(byteArray);
-byteArray = s.getBytes();
-x = new BigInteger(byteArray);
+String s = x.toString(); // Valid character data
+byte[] byteArray = s.getBytes();
+String ns = new String(byteArray);
+x = new BigInteger(ns);
 
 BigInteger x = new BigInteger("530500452766");
 String s = x.toString();  // Valid character data

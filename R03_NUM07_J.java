cec61dbe028ab80ed135f1c3d9af4a9d1f44fcce
vrushalipatel28 <vrushalipatel2004@gmail.com>
2025-03-02 21:46:54 -0500

    Update R03_NUM07_J.java

--- R03_NUM07_J.java
+++ R03_NUM07_J.java
@@ -2,7 +2,7 @@ public class NaNComparison {
   public static void main(String[] args) {
     double x = 0.0;
     double result = Math.cos(1/x); // Returns NaN if input is infinity
-    if (result == Double.NaN) { // Comparison is always false!
+    if (Double.isNaN(result)) {
       System.out.println("result is NaN");
     }
   }
